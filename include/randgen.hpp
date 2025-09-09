997e3d157dd8ea503930bbe888d5bf5e395ade78
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2025-09-09 12:58:09 +0100

    Add `float` to `randgen`
    * Add generator functions for 32-, 64-, and 128- (I think?) bit floats,
      corresponding to `float`, `double`, and `long double` (I think?)
    * Abstract types in `randgen` somewhat

--- include/randgen.hpp
+++ include/randgen.hpp
@@ -5,25 +5,30 @@
 #include <memory>
 #include <random>
 
-constexpr uint8_t max_bit_width = 64;
+constexpr uint8_t max_bit_width_int = 64;
+constexpr uint8_t max_bit_width_fl = 128;
 
 class IF_Randgen
 {
 private:
+    using max_int_t = int64_t;
+    using max_uint_t = uint64_t;
+    using max_fl_t = long double;
+    
     std::mt19937_64 random_engine;
-    std::uniform_int_distribution<int64_t> int_dist;
-    std::uniform_real_distribution<double> real_dist;
+    std::uniform_int_distribution<max_int_t> int_dist;
+    std::uniform_real_distribution<max_fl_t> real_dist;
 
 public:
     IF_Randgen();
     IF_Randgen(int);
 
-    int64_t gen_signed_int(uint8_t = max_bit_width);
-    uint64_t gen_unsigned_int(uint8_t = max_bit_width);
+    max_int_t gen_signed_int(uint8_t = max_bit_width_int);
+    max_uint_t gen_unsigned_int(uint8_t = max_bit_width_int);
+
+    max_fl_t gen_float(uint8_t = max_bit_width_fl);
 
     template <typename T> T gen(void);
-
-    double gen_double() { return this->real_dist(this->random_engine); };
 };
 
 #endif // _IF_RANDGEN_HPP
--- src/randgen.cpp
+++ src/randgen.cpp
@@ -2,10 +2,10 @@
 
 IF_Randgen::IF_Randgen() :
     random_engine(),
-    real_dist(
-        std::numeric_limits<double>::min(), std::numeric_limits<double>::max()),
-    int_dist(std::numeric_limits<int64_t>::min(),
-        std::numeric_limits<int64_t>::max()) { };
+    real_dist(std::numeric_limits<max_fl_t>::min(),
+        std::numeric_limits<max_fl_t>::max()),
+    int_dist(std::numeric_limits<max_int_t>::min(),
+        std::numeric_limits<max_int_t>::max()) { };
 
 IF_Randgen::IF_Randgen(int seed) :
     IF_Randgen()
@@ -13,16 +13,16 @@ IF_Randgen::IF_Randgen(int seed) :
     random_engine.seed(seed);
 }
 
-int64_t
+IF_Randgen::max_int_t
 IF_Randgen::gen_signed_int(uint8_t bit_width)
 {
-    if (bit_width > max_bit_width)
+    if (bit_width > max_bit_width_int)
     {
         throw std::runtime_error("Requested bit width larger than max set!");
     }
 
     decltype(this->int_dist)::param_type p;
-    if (bit_width == max_bit_width)
+    if (bit_width == max_bit_width_int)
     {
         p = this->int_dist.param();
     }
@@ -34,11 +34,49 @@ IF_Randgen::gen_signed_int(uint8_t bit_width)
     return this->int_dist(this->random_engine, p);
 }
 
-uint64_t
+IF_Randgen::max_uint_t
 IF_Randgen::gen_unsigned_int(uint8_t bit_width)
 {
     return (uint64_t) this->gen_signed_int(bit_width);
 }
+
+IF_Randgen::max_fl_t
+IF_Randgen::gen_float(uint8_t bit_width)
+{
+    decltype(this->real_dist)::param_type p;
+    switch (bit_width)
+    {
+        case 32:
+            p = decltype(this->real_dist)::param_type {
+                std::numeric_limits<float>::min(),
+                std::numeric_limits<float>::max()
+            };
+            break;
+        case 64:
+            p = decltype(this->real_dist)::param_type {
+                std::numeric_limits<double>::min(),
+                std::numeric_limits<double>::max()
+            };
+            break;
+        case max_bit_width_fl:
+            p = this->real_dist.param();
+            // p = decltype(this->real_dist)::param_type {
+            // std::numeric_limits<long double>::min(),
+            // std::numeric_limits<long double>::max()
+            //};
+            break;
+        default:
+            throw std::runtime_error(
+                "Unexpected float bit-size: " + std::to_string(bit_width));
+    }
+    return this->real_dist(this->random_engine, p);
+}
+
+/*******************************************************************************
+ * Specialised `gen` functions
+ ******************************************************************************/
+
+/* 64 bits ********************************************************************/
 
 template <>
 uint64_t
@@ -54,9 +92,70 @@ IF_Randgen::gen<int64_t>(void)
     return this->gen_signed_int(64);
 }
 
+/* 32 bits ********************************************************************/
+
+template <>
+uint32_t
+IF_Randgen::gen<uint32_t>(void)
+{
+    return this->gen_unsigned_int(32);
+}
+
+template <>
+int32_t
+IF_Randgen::gen<int32_t>(void)
+{
+    return this->gen_signed_int(32);
+}
+
+/* 16 bits ********************************************************************/
+
+template <>
+uint16_t
+IF_Randgen::gen<uint16_t>(void)
+{
+    return this->gen_unsigned_int(16);
+}
+
+template <>
+int16_t
+IF_Randgen::gen<int16_t>(void)
+{
+    return this->gen_signed_int(16);
+}
+
+/*  8 bits ********************************************************************/
+
 template <>
 uint8_t
 IF_Randgen::gen<uint8_t>(void)
 {
     return this->gen_unsigned_int(8);
+}
+
+/* float **********************************************************************/
+
+template <>
+float
+IF_Randgen::gen<float>(void)
+{
+    return this->gen_float(32);
+}
+
+/* double *********************************************************************/
+
+template <>
+double
+IF_Randgen::gen<double>(void)
+{
+    return this->gen_float(64);
+}
+
+/* long double ****************************************************************/
+
+template <>
+long double
+IF_Randgen::gen<long double>(void)
+{
+    return this->gen_float(128); // TODO double check size here, might be 80
 }
