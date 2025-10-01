20a30ecbf74977fcced1a2988e726797a9c6c068
Andrei Lascu <a.lascu@ucl.ac.uk>
2025-10-01 16:24:25 +0100

    Add `fmt` to build
    * Adjust `CMakeLists` files to build and correctly add dependencies for
      the new `fmt` third party library
    * Update `instr_emulator-typemap.hpp` to make use of `fmt` for
      `fn_sig_map` (more to come)

--- include/instr_emulator-typemap.hpp
+++ include/instr_emulator-typemap.hpp
@@ -1,3 +1,5 @@
+#include "fmt/format.h"
+
 /* Emulated instruction types *************************************************/
 #include <unordered_map>
 using fn_ty_binop_int = std::function<int64_t(int64_t, int64_t)>;
@@ -22,12 +24,24 @@ enum fn_enum
 };
 
 /* Signature to enum map ******************************************************/
+const std::string int_ty_str = "i64";
+const std::string flt_ty_str = "double";
+const std::string bool_ty_str = "i1";
+
 const std::unordered_map<std::string, fn_enum> fn_sig_map {
-    { std::string("(i64)[i64,i64]"), fn_enum::BINOP_INT },
-    { std::string("(double)[double,double]"), fn_enum::BINOP_FLT },
-    { std::string("(i1)[i64,i64]"), fn_enum::CMP_FLT },
-    { std::string("(i1)[double,double]"), fn_enum::CMP_FLT },
-    { std::string("(i64)[float]"), fn_enum::CONV_FLT_TO_INT },
+    //{ std::string("(i64)[i64,i64]"), fn_enum::BINOP_INT },
+    //{ std::string("(double)[double,double]"), fn_enum::BINOP_FLT },
+    //{ std::string("(i1)[i64,i64]"), fn_enum::CMP_FLT },
+    //{ std::string("(i1)[double,double]"), fn_enum::CMP_FLT },
+    //{ std::string("(i64)[float]"), fn_enum::CONV_FLT_TO_INT },
+    { fmt::format("({0})[{0},{0}]", int_ty_str), fn_enum::BINOP_INT },
+    { fmt::format("({0})[{0},{0}]", flt_ty_str), fn_enum::BINOP_FLT },
+    { fmt::format("({0})[{1},{1}]", bool_ty_str, int_ty_str),
+        fn_enum::CMP_INT },
+    { fmt::format("({0})[{1},{1}]", bool_ty_str, flt_ty_str),
+        fn_enum::CMP_FLT },
+    { fmt::format("({0})[{1}]", int_ty_str, flt_ty_str),
+        fn_enum::CONV_FLT_TO_INT },
 };
 
 using fn_ty = std::variant<fn_ty_binop_int, fn_ty_binop_flt, fn_ty_cmp_int,
