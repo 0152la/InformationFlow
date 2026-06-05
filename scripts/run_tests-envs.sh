27febef61d53dd4ccc0f578fd8e40b539687ee28
Andrei Lascu <a.lascu@ucl.ac.uk>
2026-06-05 10:43:14 +0100

    Slight updates
    * Add a `minimal-cycle` test, and build it as part of tests
    * More progress on `llvm_instrs.ods` table
    * Improve `run_tests-envs.sh`, and add runtime config files

--- scripts/run_tests-envs.sh
+++ scripts/run_tests-envs.sh
@@ -1,4 +1,9 @@
-export IF_BUILD_DIR=/home/andreilascu/Documents/Repos/InformationFlow/build/
+export IF_ROOT_DIR=/home/andreilascu/Documents/Repos/InformationFlow
+
+export IF_BUILD_DIR=$IF_ROOT_DIR/build
 export IF_LLVM_TEST_FILE=$IF_BUILD_DIR/tests/simple-arith.ll
 export IF_LLVM_SNIP_LIB=$IF_BUILD_DIR/src/libllvm_snippets.so
 export IF_ENTROPY_TEST_FILE=$IF_BUILD_DIR/tests/entropy-func_loss-exec
+
+export IF_SET_ENTROPY_FILE=$IF_ROOT_DIR/configs/entropy_set.toml
+export IF_EMULATED_ENTROPY_FILE=$IF_ROOT_DIR/configs/all.csv
